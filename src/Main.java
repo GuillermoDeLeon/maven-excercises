538a301b45a8a170ec2648215faddc4a04f102e4
Guillermo De Leon <guillermo.x.deleon@gmail.com>
2020-04-21 20:30:15 -0500

    added maven exercises

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter something: ");
+        String userInput = sc.nextLine();
+
+        System.out.println(userInput);
+
+    }
+}
